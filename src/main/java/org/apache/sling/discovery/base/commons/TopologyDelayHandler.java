01925d7f14587e6ace8c6371f28d62013f510f40
ionutzpi <ionutzpi@yahoo.com>
2025-04-16 16:16:53 +0300

    [SLING-12743]--Add topology delay mechanism to ensure system stability during topology changes.

--- src/main/java/org/apache/sling/discovery/base/commons/TopologyDelayHandler.java
+++ src/main/java/org/apache/sling/discovery/base/commons/TopologyDelayHandler.java
@@ -30,7 +30,6 @@ import org.osgi.service.component.annotations.ReferenceCardinality;
 import org.osgi.service.component.annotations.ReferencePolicy;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.osgi.framework.ServiceReference;
 import org.osgi.service.component.ComponentContext;
 
 /**
